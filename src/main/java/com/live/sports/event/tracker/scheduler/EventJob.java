c25ebf931c493c3c5f35f1dc894a79da5b82ded0
Dimitris <nomix_xx@hotmai.com>
2025-06-27 22:23:19 +0100

    Publish live event scores to kafka topic

--- src/main/java/com/live/sports/event/tracker/scheduler/EventJob.java
+++ src/main/java/com/live/sports/event/tracker/scheduler/EventJob.java
@@ -1,7 +1,5 @@
 package com.live.sports.event.tracker.scheduler;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.live.sports.event.tracker.domain.Event;
 import com.live.sports.event.tracker.domain.EventStatus;
 import com.live.sports.event.tracker.repository.EventRepository;
@@ -9,11 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -21,8 +19,9 @@ public class EventJob implements Job {
 
     private final EventRepository eventRepository;
     private final RestTemplate restTemplate = new RestTemplate();
-    ObjectMapper objectMapper = new ObjectMapper();
     private final String URL = "http://localhost:8080/mockScore";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC = "score-topic";
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -30,13 +29,7 @@ public class EventJob implements Job {
 
         for(Event event: events) {
             String response = restTemplate.getForObject(URL +"/"+event.getEventId() , String.class);
-            System.out.println("executing job for"+ event.getEventId());
-            try {
-                System.out.println(response);
-                Event parsedEvent = objectMapper.readValue(response, Event.class);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
+            kafkaTemplate.send(TOPIC, response);
         }
     }
 }
